5270a2f9656d14b7ca134e1c4c5e405a1aaa0eb1
grakify90 <ninavanes1990@hotmail.com>
2020-07-17 18:03:40 +0200

    ran some tests by making queries

--- models/customer.js
+++ models/customer.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      category.hasMany(models.order);
+      customer.hasMany(models.order);
     }
   }
   customer.init(
--- testQueries.js
+++ testQueries.js
@@ -1,5 +1,59 @@
-const Costumer = require("./models").costumer;
-const Category = require("./models").category;
-const Order = require("./models").order;
-const Product = require("./models").product;
-const OrderProduct = require("./models").orderProduct;
+const {
+  customer,
+  category,
+  order,
+  product,
+  productOrder,
+} = require("./models");
+
+async function customersWithOrders() {
+  const custWithOrders = await customer.findAll({ include: [order] });
+  return custWithOrders.map((list) => list.get({ plain: true }));
+}
+//customersWithOrders().then((lists) => console.log(lists));
+
+async function ordersWithCustomer() {
+  const custWithOrders = await order.findAll({ where: { customerId: 3 } });
+  console.log(custWithOrders);
+}
+//specificCustomerWithOrders();
+
+async function productListNames() {
+  const list = await product.findAll();
+  return list.map((item) => item.get({ plain: true }));
+}
+productListNames().then((list) => console.log(list));
+
+async function productWithCategory() {
+  const specificProduct = await product.findByPk(1, {
+    include: [category],
+  });
+  console.log(specificProduct);
+}
+//productWithCategory();
+
+async function productOrderList() {
+  const listProductOrders = await productOrder.findAll();
+  return listProductOrders.map((item) => item.get({ plain: true }));
+}
+//productOrderList().then((list) => console.log(list));
+
+async function customerWithProducts() {
+  const result = await customer.findByPk(2, {
+    include: [
+      {
+        model: order,
+        include: { model: product, attributes: ["name"] },
+      },
+    ],
+  });
+  return result.get({ plain: true });
+}
+//customerWithProducts().then((item) => console.log(item));
+
+// Many to many query
+async function ordersWithProducts() {
+  const orderList = await order.findAll({ include: [product] });
+  return orderList.map((item) => item.get({ plain: true }));
+}
+//ordersWithProducts().then((items) => console.log(items));
