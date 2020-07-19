3306056e6e91123a9cb30710cd14b0383c2ba92f
grakify90 <ninavanes1990@hotmail.com>
2020-07-19 12:49:38 +0200

    created two jwt functions

---
+++ authorization/jwt.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+const secretKey = process.env JWT_SECRET || "nqeoir209en0870cwjsue9h827";
+
+function toJWT(data){
+    return jwt.sign(data, secretKey, { expiresIn: "2h" });
+}
+
+function toData(jwt){
+    return jwt.verify(jwt, secretKey);
+}
+
+module.exports = { toJWT, toData };
