ddc1379cf4984c72aa6d1ce044f1775310a785f2
grakify90 <ninavanes1990@hotmail.com>
2020-07-23 15:51:43 +0200

    set up for heroku

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 4000;
 const jsonParser = express.json();
 const customerRouter = require("./routers/customers");
 const productRouter = require("./routers/products");
 const authRouter = require("./routers/authorization");
+const cors = require("cors");
+app.use(cors());
 
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.use(jsonParser);
 
